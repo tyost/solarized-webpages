c0084a42a89bc179c053d1d01f790a16bbc24635
Tim Yost <tyost@users.noreply.github.com>
2016-03-22 23:45:02 -0400

    Include anchors (hyperlinks) in bordered interactive elements.

--- solarized-webpages.user.js
+++ solarized-webpages.user.js
@@ -226,7 +226,7 @@ var onLoad = function() {
   };
 
   var getInteractiveElementCss = function() {
-    var css = 'applet, button, command, datalist, details, ' +
+    var css = 'a, applet, button, command, datalist, details, ' +
               'dialog, dir, input, isindex, keygen, ' +
               'listing, menu, menuitem, meter, optgroup, option, output, ' +
               'select, summary, textarea, ' +
