d9e590d19e7c6c33b3d19b88f2f10c2730229d2d
Tim Yost <tyost@users.noreply.github.com>
2016-04-09 22:39:52 -0400

    Initial conversion to TypeScript.

--- src/solarized-webpages.user.ts
+++ src/solarized-webpages.user.ts
@@ -1,28 +1,24 @@
-// ==UserScript==
-// @name        Solarized Webpages
-// @namespace   tyost
-// @description Adjusts websites to fit the Solarized color palette.
-// @include     *
-// @include     about:blank#solarized-config
-// @version     0.1.1
-// @grant       GM_addStyle
-// @grant       GM_setValue
-// @grant       GM_getValue
-// @run-at      document-start
-// ==/UserScript==
+/// <reference path="../lib/es6-shim.d.ts" />
+/// <reference path="./Greasemonkey.ts" />
 
-var onLoad = function() {
+let onLoad = function() {
   'use strict';
+
+  //======================================
+  // Greasemonkey
+  //======================================
+
+  let greasemonkey = new Greasemonkey();
 
   //======================================
   // Set defaults for missing configuration settings.
   //======================================
 
-  var setDefault = function(setting, defaultValue) {
-    GM_setValue(setting, GM_getValue(setting, defaultValue));
+  let setDefault = function(setting, defaultValue) {
+    greasemonkey.setValue(setting, greasemonkey.getValue(setting, defaultValue));
   };
 
-  var setupDefaultConfiguration = function() {
+  let setupDefaultConfiguration = function() {
     setDefault('colorTheme', 'dark');
   };
 
@@ -38,39 +34,44 @@ var onLoad = function() {
   // element. As a result, they compare the entire class string against one
   // class name. That code would break if we added another class.
 
-  var COLOR_GRADIENT_REGEX =
+  let COLOR_GRADIENT_REGEX =
       /^(-(moz|ms|o|webkit)-)?(linear-gradient|repeating-linear-gradient|radial-gradient|repeating-radial-gradient)/;
 
-  var isColorGradient = function(value) {
+  let isColorGradient = function(value) {
     return COLOR_GRADIENT_REGEX.test(value);
   };
 
-  var setAttributeIfDifferent = function(element, attribute, value) {
+  let setAttributeIfDifferent = function(element, attribute, value) {
     if (element.getAttribute(attribute) != value) {
       element.setAttribute(attribute, value);
     }
   };
 
-  var markIfHasBackgroundColor = function(element, computedStyle) {
-    var backgroundImage = computedStyle.getPropertyValue('background-image');
+  let markIfHasBackgroundColor = function(element, computedStyle) {
+    let backgroundImage = computedStyle.getPropertyValue('background-image');
 
     if (backgroundImage && backgroundImage !== 'none' &&
           !isColorGradient(backgroundImage)) {
       return;
     }
 
-    var backgroundColor = computedStyle.getPropertyValue('background-color');
+    let backgroundColor = computedStyle.getPropertyValue('background-color');
     if (backgroundColor && backgroundColor !== 'transparent') {
       setAttributeIfDifferent(element, 'data-has-background-color-before-solarized', '');
     }
   };
 
-  var markElementsForCss = function() {
-    var allElements = document.getElementsByTagName('*');
+  let getComputedStylePropertyRgb = function(computedStyle, property) {
+    let color = computedStyle.getPropertyCSSValue('color');
+    return color ? color.getRGBColorValue() : null;
+  };
 
-    for (var i = allElements.length; i--;) {
-      var element = allElements[i];
-      var computedStyle = window.getComputedStyle(element, null);
+  let markElementsForCss = function() {
+    let allElements = document.getElementsByTagName('*');
+
+    for (let i = allElements.length; i--;) {
+      let element = allElements[i];
+      let computedStyle = window.getComputedStyle(element, null);
 
       if (computedStyle) {
         markIfHasBackgroundColor(element, computedStyle);
@@ -87,7 +88,7 @@ var onLoad = function() {
   // Functions for retrieving certain DOM elements.
   //======================================
 
-  var getHtmlElement = function() {
+  let getHtmlElement = function() {
     return document.getElementsByTagName('html')[0];
   };
 
@@ -95,9 +96,9 @@ var onLoad = function() {
   // Re-scan and mark the page when something changes that might affect styles.
   //======================================
 
-  var registerForDomChange = function(callback) {
-    var observer = new MutationObserver(callback);
-    var observerSettings = {
+  let registerForDomChange = function(callback) {
+    let observer = new MutationObserver(callback);
+    let observerSettings = {
       attributes: true,
       characterData: true,
       childList: true,
@@ -107,11 +108,11 @@ var onLoad = function() {
   };
 
   // Time in miliseconds to wait before scanning and marking when scheduled.
-  var REMARK_DELAY = 100;
+  let REMARK_DELAY = 100;
 
-  var remarkTimeoutId = false;
+  let remarkTimeoutId: boolean|number = false;
 
-  var scheduleRemarking = function() {
+  let scheduleRemarking = function() {
     if (!remarkTimeoutId) {
       remarkTimeoutId = window.setTimeout(
         function() {
@@ -123,7 +124,7 @@ var onLoad = function() {
     }
   };
 
-  var setupRemarking = function() {
+  let setupRemarking = function() {
     registerForDomChange(scheduleRemarking);
   };
 
@@ -133,7 +134,7 @@ var onLoad = function() {
   // Colors.
   //======================================
 
-  var SOLARIZED_PALETTE = {
+  let SOLARIZED_PALETTE = {
     BASE03:   '#002b36',   // Darker
     BASE02:   '#073642',
     BASE01:   '#586e75',
@@ -152,8 +153,8 @@ var onLoad = function() {
     YELLOW:   '#b58900'
   };
 
-  // Base colors varying between light and dark.
-  var COLOR_THEMES = {
+  // Base colors letying between light and dark.
+  let COLOR_THEMES = {
     light: {
       BACKGROUND:           SOLARIZED_PALETTE.BASE3,
       BACKGROUND_HIGHLIGHT: SOLARIZED_PALETTE.BASE2,
@@ -167,7 +168,7 @@ var onLoad = function() {
   };
 
   // Choose colors based on the user's theme setting.
-  var COLORS = Object.assign({}, COLOR_THEMES[GM_getValue('colorTheme')]);
+  let COLORS = Object.assign({}, COLOR_THEMES[greasemonkey.getValue('colorTheme')]);
 
   // Color settings shared between light and dark.
   COLORS.HEADINGS = SOLARIZED_PALETTE.YELLOW;
@@ -178,18 +179,18 @@ var onLoad = function() {
   // Setup elements needed to control specificity (precedence) of CSS code.
   //======================================
 
-  var getHtmlId = function() {
+  let getHtmlId = function() {
     return getHtmlElement().getAttribute('id');
   };
 
-  var isOnlyWhitespace = function(s) {
+  let isOnlyWhitespace = function(s) {
     return /^\s*$/.test(s);
   };
 
-  var DEFAULT_HTML_ID = 'solarizedHtml54321';
+  let DEFAULT_HTML_ID = 'solarizedHtml54321';
 
-  var setHtmlIdIfMissing = function() {
-    var htmlId = getHtmlId();
+  let setHtmlIdIfMissing = function() {
+    let htmlId = getHtmlId();
 
     if (!htmlId || isOnlyWhitespace(htmlId)) {
       getHtmlElement().setAttribute('id', DEFAULT_HTML_ID);
@@ -202,30 +203,30 @@ var onLoad = function() {
   // Functions controlling the specificity (precedence) of CSS code.
   //======================================
 
-  var buildIdSelector = function(id) {
+  let buildIdSelector = function(id) {
     return '#' + id;
   };
 
-  var buildIdSelectorWithSpecificity = function(id, amount) {
+  let buildIdSelectorWithSpecificity = function(id, amount) {
     return buildIdSelector(id).repeat(amount);
   };
 
-  var getSubstringBefore = function(s, character) {
+  let getSubstringBefore = function(s, character) {
     return s.substring(0, s.indexOf(character));
   };
 
-  var insertBeforeAllSelectors = function(css, extraSelector) {
-    var selectorCss = getSubstringBefore(css, '{');
-    var newSelectorCss = selectorCss.replace(/([^,]+)(,|$)/g, extraSelector + ' $1$2');
+  let insertBeforeAllSelectors = function(css, extraSelector) {
+    let selectorCss = getSubstringBefore(css, '{');
+    let newSelectorCss = selectorCss.replace(/([^,]+)(,|$)/g, extraSelector + ' $1$2');
     return css.replace(selectorCss, newSelectorCss);
   };
 
-  var increaseAllSpecificity = function(css, amount) {
+  let increaseAllSpecificity = function(css, amount) {
     // Insert a repeated ID before each CSS selector to artificially raise
     // its specificity by the specified amount.
     // For example, #solarizedHtml54321#solarizedHtml54321 p
     // raises the id specificity by two.
-    var extraSelector = buildIdSelectorWithSpecificity(getHtmlId(), amount);
+    let extraSelector = buildIdSelectorWithSpecificity(getHtmlId(), amount);
     return insertBeforeAllSelectors(css, extraSelector);
   };
 
@@ -233,69 +234,81 @@ var onLoad = function() {
   // Recolor the page with CSS.
   //======================================
 
-  var GENERIC_SPECIFICITY = 15;
-  var HIGHLIGHT_SPECIFICITY = GENERIC_SPECIFICITY + 1;
+  let GENERIC_SPECIFICITY = 15;
+  let HIGHLIGHT_SPECIFICITY = GENERIC_SPECIFICITY + 1;
 
-  var getGenericCss = function() {
-    var css = '* {' +
-              ' border-color: ' + COLORS.BACKGROUND_HIGHLIGHT + ' !important;' +
-              ' color: ' + COLORS.BODY_TEXT + ' !important;' +
-              ' text-shadow: none !important;' +
-              '}';
+  let getGenericCss = function() {
+    let css = `
+        * {
+          border-color: ${COLORS.BACKGROUND_HIGHLIGHT} !important;
+          color: ${COLORS.BODY_TEXT} !important;
+          text-shadow: none !important;
+        }
+    `;
     return increaseAllSpecificity(css, GENERIC_SPECIFICITY);
   };
 
-  var getColoredBackgroundCss = function() {
-    var css = 'body, ' +
-              '[data-has-background-color-before-solarized] {' +
-              ' background-color: ' + COLORS.BACKGROUND + ' !important;' +
-              ' background-image: none !important;' +
-              '}';
+  let getColoredBackgroundCss = function() {
+    let css = `
+        body,
+        [data-has-background-color-before-solarized] {
+          background-color: ${COLORS.BACKGROUND} !important;
+          background-image: none !important;
+        }
+    `;
     return increaseAllSpecificity(css, GENERIC_SPECIFICITY);
   };
 
-  var getHeadingCss = function() {
-    var css = 'h1, h2, h3, h4, h5, h6, header, hgroup, thead,' +
-              'h1 *, h2 *, h3 *, h4 *, h5 *, h6 *, header *, hgroup *, thead * {' +
-              ' color: ' + COLORS.HEADINGS + ' !important;' +
-              '}';
+  let getHeadingCss = function() {
+    let css = `
+        h1, h2, h3, h4, h5, h6, header, hgroup, thead,
+        h1 *, h2 *, h3 *, h4 *, h5 *, h6 *, header *, hgroup *, thead * {
+          color: ${COLORS.HEADINGS} !important;
+        }
+    `;
     return increaseAllSpecificity(css, GENERIC_SPECIFICITY);
   };
 
-  var getHyperlinkCss = function() {
-    var css = 'a {' +
-              ' color: ' + COLORS.HYPERLINKS + ' !important;' +
-              '}';
+  let getHyperlinkCss = function() {
+    let css = `
+        a {
+          color: ${COLORS.HYPERLINKS} !important;
+        }
+    `;
     return increaseAllSpecificity(css, GENERIC_SPECIFICITY);
   };
 
-  var getHighlightCss = function() {
-    var css = 'a[data-has-background-color-before-solarized], ' +
-              'applet, button, code, command, datalist, details, ' +
-              'dialog, dir, frame, frameset, input, isindex, keygen, legend, ' +
-              'listing, menu, menuitem, meter, optgroup, option, output, pre, progress, ' +
-              'select, summary, textarea {' +
-              ' background-color: ' + COLORS.BACKGROUND_HIGHLIGHT + ' !important;' +
-              ' opacity: 1 !important;' +
-              '}';
+  let getHighlightCss = function() {
+    let css = `
+        a[data-has-background-color-before-solarized],
+        applet, button, code, command, datalist, details,
+        dialog, dir, frame, frameset, input, isindex, keygen, legend,
+        listing, menu, menuitem, meter, optgroup, option, output, pre, progress,
+        select, summary, textarea {
+          background-color: ${COLORS.BACKGROUND_HIGHLIGHT} !important;
+          opacity: 1 !important;
+        }
+    `;
     return increaseAllSpecificity(css, HIGHLIGHT_SPECIFICITY);
   };
 
-  var getInteractiveElementCss = function() {
-    var css = 'a[data-has-background-color-before-solarized], ' +
-              'applet, button, command, datalist, details, ' +
-              'dialog, dir, input, isindex, keygen, ' +
-              'listing, menu, menuitem, meter, optgroup, option, output, ' +
-              'select, summary, textarea, ' +
-              '[role="button"], [role="checkbox"], [role="radio"], ' +
-              '[role="scrollbar"], [role="slider"], [role="spinbutton"], ' +
-              '[role="switch"], [role="textbox"] {' +
-              ' border: 1px dotted ' + COLORS.INTERACTIVE_ELEMENT_BORDER + ' !important;' +
-              '}';
+  let getInteractiveElementCss = function() {
+    let css = `
+        a[data-has-background-color-before-solarized],
+        applet, button, command, datalist, details,
+        dialog, dir, input, isindex, keygen,
+        listing, menu, menuitem, meter, optgroup, option, output,
+        select, summary, textarea,
+        [role="button"], [role="checkbox"], [role="radio"],
+        [role="scrollbar"], [role="slider"], [role="spinbutton"],
+        [role="switch"], [role="textbox"] {
+          border: 1px dotted ${COLORS.INTERACTIVE_ELEMENT_BORDER} !important;
+        }
+    `;
     return increaseAllSpecificity(css, HIGHLIGHT_SPECIFICITY);
   };
 
-  var getAllCss = function() {
+  let getAllCss = function() {
     return  getGenericCss() +
             getColoredBackgroundCss() +
             getHeadingCss() +
@@ -304,36 +317,36 @@ var onLoad = function() {
             getInteractiveElementCss();
   };
 
-  GM_addStyle(getAllCss());
+  greasemonkey.addStyle(getAllCss());
 
   //======================================
   // Configuration page to edit the script's settings.
   //======================================
 
-  var createH1 = function(text) {
-    var h1 = document.createElement('h1');
+  let createH1 = function(text) {
+    let h1 = document.createElement('h1');
 
     h1.innerHTML = text;
 
     return h1;
   };
 
-  var createLabel = function(forId, text) {
-    var label = document.createElement('label');
+  let createLabel = function(forId, text) {
+    let label = document.createElement('label');
 
-    label.for = forId;
+    label.setAttribute('for', forId);
     label.innerHTML = text;
 
     return label;
   };
 
-  var createSelect = function(id, defaultValue, options) {
-    var select = document.createElement('select');
+  let createSelect = function(id, defaultValue, options) {
+    let select = document.createElement('select');
 
-    for (var value in options) {
-      var text = options[value];
+    for (let value in options) {
+      let text = options[value];
 
-      var option = document.createElement('option');
+      let option = document.createElement('option');
 
       option.value = value;
       option.text = text;
@@ -348,24 +361,24 @@ var onLoad = function() {
     return select;
   };
 
-  var getBody = function() {
+  let getBody = function() {
     return document.getElementsByTagName('body')[0];
   };
 
-  var appendToForm = function(elem) {
+  let appendToForm = function(elem) {
     getBody().appendChild(elem);
   };
 
-  var setupcolorThemeSelect = function() {
-    var colorThemeLabel = createLabel(
+  let setupcolorThemeSelect = function() {
+    let colorThemeLabel = createLabel(
       'color-theme-select',
       'Color Theme'
     );
     appendToForm(colorThemeLabel);
 
-    var colorThemeSelect = createSelect(
+    let colorThemeSelect = createSelect(
       'color-theme-select',
-      GM_getValue('colorTheme'),
+      greasemonkey.getValue('colorTheme'),
       {
         light:  'Light',
         dark:   'Dark'
@@ -374,26 +387,26 @@ var onLoad = function() {
     appendToForm(colorThemeSelect);
 
     colorThemeSelect.addEventListener('change', function() {
-      GM_setValue('colorTheme', colorThemeSelect.value);
+      greasemonkey.setValue('colorTheme', colorThemeSelect.value);
     });
   };
 
-  var setupForm = function() {
+  let setupForm = function() {
     appendToForm(createH1('Solarized Webpages Configuration'));
     setupcolorThemeSelect();
   };
 
 
-  var getUrl = function() {
+  let getUrl = function() {
     return window.location.href;
   };
 
-  var scriptAlreadyRan = function() {
+  let scriptAlreadyRan = function() {
     return !!getBody().children.length;
   };
 
 
-  var CONFIG_FORM_URL = 'about:blank#solarized-config';
+  let CONFIG_FORM_URL = 'about:blank#solarized-config';
 
   if (getUrl() === CONFIG_FORM_URL && !scriptAlreadyRan()) {
     setupForm();
